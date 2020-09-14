4920de369c9ef9390cc84dc137af4bcd7a337739
clobee <clobee@users.noreply.github.com>
2020-09-14 12:36:24 +0100

    Adds basic port scanner

---
+++ port-scanner.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	for i := 0; i <= 1024; i++ {
+		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+		conn, err := net.Dial("tcp", address)
+
+		if err != nil {
+			// Port closed / filtered
+			continue
+		}
+
+		conn.Close()
+		fmt.Printf("%d is open \n", i)
+	}
+}
