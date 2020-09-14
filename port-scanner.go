eaf182672ca76a54bce1803923bab51821dff312
clobee <clobee@users.noreply.github.com>
2020-09-14 13:36:01 +0100

    Adds waitgroup

--- port-scanner.go
+++ port-scanner.go
@@ -3,19 +3,31 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
 	for i := 0; i <= 1024; i++ {
-		address := fmt.Sprintf("scanme.nmap.org:%d", i)
-		conn, err := net.Dial("tcp", address)
-
-		if err != nil {
-			// Port closed / filtered
-			continue
-		}
-
-		conn.Close()
-		fmt.Printf("%d is open \n", i)
+		wg.Add(1)
+		go scanPort(i, &wg)
 	}
+	wg.Wait()
+}
+
+func scanPort(p int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Printf("Scan of %d starting... \n", p)
+
+	address := fmt.Sprintf("scanme.nmap.org:%d", p)
+	conn, err := net.Dial("tcp", address)
+
+	if err != nil {
+		// Port closed / filtered
+		return
+	}
+
+	conn.Close()
+	fmt.Printf("%d is open \n", p)
 }
