f2b704933fdb09e51b7b0265362851387369673f
clobee <clobee@users.noreply.github.com>
2020-09-17 20:33:02 +0100

    Uses multiple channels

--- port-scanner.go
+++ port-scanner.go
@@ -3,39 +3,51 @@ package main
 import (
 	"fmt"
 	"net"
-	"sync"
+	"sort"
 )
 
 func main() {
 	ports := make(chan int, 100)
-	var wg sync.WaitGroup
+	results := make(chan int)
+	var openports []int
 
 	for i := 0; i < cap(ports); i++ {
-		go scanPort(ports, &wg)
+		go scanPort(ports, results)
 	}
+
+	go func() {
+		for i := 1; i <= 1024; i++ {
+			ports <- i
+		}
+	}()
 
 	for i := 1; i <= 1024; i++ {
-		wg.Add(1)
-		ports <- i
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		}
 	}
 
-	wg.Wait()
 	close(ports)
+	close(results)
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("%d open\n", port)
+	}
 }
 
-func scanPort(ports chan int, wg *sync.WaitGroup) {
+func scanPort(ports, results chan int) {
 	for p := range ports {
 
 		address := fmt.Sprintf("scanme.nmap.org:%d", p)
 		conn, err := net.Dial("tcp", address)
 
 		if err != nil {
-			// Port closed / filtered
-			return
+			results <- 0
+			continue
 		}
 
 		conn.Close()
-		fmt.Printf("%d is open \n", p)
-		wg.Done()
+		results <- p
 	}
 }
