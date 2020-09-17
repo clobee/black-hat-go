12bb1b8738a9a0d64fdfed475e1e1b0a0515b1c4
clobee <clobee@users.noreply.github.com>
2020-09-17 21:01:36 +0100

    Adds reader/writer system

---
+++ reader_writer.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+// FooReader defines an io.Reader to read from Stdin
+type FooReader struct{}
+
+func (fooReader *FooReader) Read(b []byte) (int, error) {
+	fmt.Print("in > ")
+	return os.Stdin.Read(b)
+}
+
+// FooWriter defines an io.Writer to write to Stdout
+type FooWriter struct{}
+
+func (fooWriter *FooWriter) Write(b []byte) (int, error) {
+	fmt.Print("out > ")
+	return os.Stdout.Write(b)
+}
+
+func main() {
+	var (
+		reader FooReader
+		writer FooWriter
+	)
+
+	// Create a buffer to hold input/output
+	input := make([]byte, 4096)
+
+	s, err := reader.Read(input)
+	if err != nil {
+		log.Fatalln("Unable to read data")
+	}
+	fmt.Printf("Read %d bytes from stdin\n", s)
+
+	s, err = writer.Write(input)
+	if err != nil {
+		log.Fatalln("Unable to write data")
+	}
+	fmt.Printf("Wrote %d bytes to stdout\n", s)
+}
